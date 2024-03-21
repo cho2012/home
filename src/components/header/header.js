5fb6190f426898c14724eb809c78c4e644da2e65
cho2012 <cho.edu.coding@gmail.com>
2024-03-21 20:21:40 +0900

    feat:0.0.2

---
+++ src/components/header/header.js
@@ -0,0 +1,19 @@
+
+import './header.css';
+
+function Header() {
+  return <div className="header" >
+    <div>홈페이지</div>
+    <nav className='nav'>
+     <div>header</div>
+     <div>slides</div>
+     <div>banner</div>
+     <div>contents</div>
+     <div>footer</div>
+    </nav>
+    <div>로그인</div>
+    </div>
+  
+}
+
+export default Header;
